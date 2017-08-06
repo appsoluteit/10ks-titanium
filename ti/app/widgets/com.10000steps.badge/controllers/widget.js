02246c3461125fb96c0a0707f6030ff4653c878f
Jason Sultana <jason.sultana01@gmail.com>
2017-08-06 18:02:07 +1000

    Implement badge widget

--- ti/app/widgets/com.10000steps.badge/controllers/widget.js
+++ ti/app/widgets/com.10000steps.badge/controllers/widget.js
@@ -1,3 +1,22 @@
 var args = $.args;
 
-console.log(args);
+(function() {
+	console.log("Loading badge. Image = ", args.image, "Counter = ", args.badge);
+	
+	if(args.image) {
+		$.icon.image = args.image;		
+	}
+	
+	if(args.badge) {
+		$.counter.text = args.badge;		
+	}
+	
+	//Expose the icon button for click events
+	$.addEventListener = function(event, callback) {
+		$.icon.addEventListener(event, callback);
+	};
+	
+	$.setBadge = function(counter) {
+		$.counter.text = counter;
+	};
+})();
