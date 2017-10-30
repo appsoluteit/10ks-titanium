28a238316eccd9789c237a2192911bd1031cfbef
Jason Sultana <jason.sultana01@gmail.com>
2017-10-31 06:19:13 +1100

    Badge fixes

--- ti/app/widgets/com.10000steps.badge/controllers/widget.js
+++ ti/app/widgets/com.10000steps.badge/controllers/widget.js
@@ -7,16 +7,37 @@ var args = $.args;
 	//On iOS, load it from the project root. Otherwise the image may be too big.
 	
 	if(Ti.Platform.osname === "android") {
-		$.icon.image = WPATH('image.png');		
+		$.icon.image = args.image;
+		//$.icon.image = WPATH('steps.png');		
+		$.badge.visible = false;
 	}
 	else {
 		if(args.image) {
 			$.icon.image = args.image;		
-		}		
-	}
-	
-	if(args.badge) {
-		$.counter.text = args.badge;		
+		}
+		
+		//Set the badge size to be based on the calculated image size (to be flexible between iPhone and iPad)
+		var iconImage = Ti.UI.createImageView({
+			image: $.icon.image,
+			height: 'auto',
+			width: 'auto'
+		}).toBlob();
+		var iconWidth = iconImage.width;
+		iconImage = null;
+		
+		Ti.API.debug("Icon width: " + iconWidth);	
+		
+		var badgeSize = iconWidth / 3;
+		
+		Ti.API.debug("Badge size: " + badgeSize);
+		$.badge.setHeight(badgeSize + 'dp');
+		$.badge.setWidth(badgeSize + 'dp');
+		$.badge.setBorderRadius(badgeSize / 2 + 'dp');	
+		
+		//Set the counter font to be 1/5 of the icon width on iOS
+		$.counter.font = {
+			fontSize: iconWidth / 5
+		};			
 	}
 	
 	if(args.top) {
@@ -29,13 +50,25 @@ var args = $.args;
 	};
 	
 	$.setBadge = function(counter) {
-		if(counter > 0) {
-			$.badge.visible = true;	
+		if(Ti.Platform.osname === "android") {
+			if(counter > 0) {
+				//$.icon.image = WPATH('steps_' + counter + '.png');
+				$.icon.image = args.image;
+			}
+			else {
+				//$.icon.image = WPATH('steps.png');	
+				$.icon.image = args.image;				
+			}
 		}
 		else {
-			$.badge.visible = false;
+			if(counter > 0) {
+				$.badge.visible = true;	
+			}
+			else {
+				$.badge.visible = false;
+			}
+			
+			$.counter.text = counter;	
 		}
-		
-		$.counter.text = counter;
 	};
 })();
