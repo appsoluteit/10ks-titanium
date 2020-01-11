dcbc223d137efaeac2445afd56a93dc0b093af00
Praj Basnet <praj.basnet@gmail.com>
2020-01-11 21:02:27 +1000

    Working Android build

--- ti/app/alloy.js
+++ ti/app/alloy.js
@@ -177,13 +177,12 @@
 //Facebook SDK Integration
 //https://github.com/appcelerator-modules/ti.facebook
 (function() {
-   //if(!Alloy.Globals.Env === 'Development') {
-	//Ti.API.warn('initialising facebook sdk');
+   if(!Alloy.Globals.Env === 'Development') {
+	Ti.API.warn('initialising facebook sdk');
 	var fb = require('facebook');
-	fb.initialize();
-	
+	fb.initialize();	
 	// Only need Pixel support, not login
-   //}
+   }
 })();
 
 //ACS
